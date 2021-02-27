a1ec3e0ddde9e209335dbdd96f255412b6917d08
Patrick Pereira <patrickeeep12@gmail.com>
2021-02-27 09:28:29 -0300

    First commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src='https://github.com/tricksil.png' alt='Patrick Silva' />
+      <div>
+        <strong>Patrick Silva</strong>
+        <p>
+          <img src='icons/level.svg' alt='Level' />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
